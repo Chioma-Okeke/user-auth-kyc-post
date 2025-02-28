36be281d16a1325b42676ae73c38ccd90660448d
Okeke Chioma Princess <okekegeraldenne@gmail.com>
2025-02-28 20:11:16 +0100

    created mongodb connection

---
+++ db/dbController.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+
+const connectDb = async (req, res) => {
+    try {
+        await mongoose.connect(process.env.DBCONNECTIONSTRING)
+    } catch (error) {
+        console.error(error)
+    }
+
+    console.log("connected to db")
+}
+
+module.exports = connectDb
--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const connectDb = require('./db/dbController')
 
 const app = express()
 require("dotenv").config()
 
 const port = process.env.port
+
+connectDb()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, console.log("Server is running..."))
